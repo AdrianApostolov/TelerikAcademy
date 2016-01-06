5fe1e00b9cd788f77aadf6e46a68298bfe945df1
AdrianApostolov <adrian.apostolov.1991@gmail.com>
2016-01-06 08:14:40 +0200

    Uncomment chat messages

--- Homeworks/End-to-end-JavaScriptApplications/02.ChatModule/main.js
+++ Homeworks/End-to-end-JavaScriptApplications/02.ChatModule/main.js
@@ -2,32 +2,32 @@ var chatDb = require('./server/controllers/ChatDb');
 
 chatDb.init();
 
-//chatDb.registerUser({ username: 'DonchoMinkov', password: '123456q' });
-//chatDb.registerUser({ username: 'NikolayKostov', password: '123456q' });
-//
-//chatDb.sendMessage({
-//    from: 'DonchoMinkov',
-//    to: 'NikolayKostov',
-//    text: 'Hey, Niki!'
-//});
-//
-//chatDb.sendMessage({
-//    from: 'NikolayKostov',
-//    to: 'DonchoMinkov',
-//    text: 'Hey, Doncho!'
-//});
-//
-//chatDb.sendMessage({
-//    from: 'NikolayKostov',
-//    to: 'DonchoMinkov',
-//    text: 'How are you today?'
-//});
-//
-//chatDb.sendMessage({
-//    from: 'DonchoMinkov',
-//    to: 'NikolayKostov',
-//    text: 'I\'m fine! You?'
-//});
+chatDb.registerUser({ username: 'DonchoMinkov', password: '123456q' });
+chatDb.registerUser({ username: 'NikolayKostov', password: '123456q' });
+
+chatDb.sendMessage({
+    from: 'DonchoMinkov',
+    to: 'NikolayKostov',
+    text: 'Hey, Niki!'
+});
+
+chatDb.sendMessage({
+    from: 'NikolayKostov',
+    to: 'DonchoMinkov',
+    text: 'Hey, Doncho!'
+});
+
+chatDb.sendMessage({
+    from: 'NikolayKostov',
+    to: 'DonchoMinkov',
+    text: 'How are you today?'
+});
+
+chatDb.sendMessage({
+    from: 'DonchoMinkov',
+    to: 'NikolayKostov',
+    text: 'I\'m fine! You?'
+});
 
 setTimeout(function () {
     chatDb.getMessages({
