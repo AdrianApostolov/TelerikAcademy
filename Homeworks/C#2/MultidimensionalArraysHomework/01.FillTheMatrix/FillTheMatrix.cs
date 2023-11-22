6ae1a34c83574a10f04ee2829832f65f3722ae83
Adrian Apostolov <adrian.apostolov.1991@gmail.com>
2023-11-22 16:25:55 +0200

    Update FillTheMatrix.cs

--- Homeworks/C#2/MultidimensionalArraysHomework/01.FillTheMatrix/FillTheMatrix.cs
+++ Homeworks/C#2/MultidimensionalArraysHomework/01.FillTheMatrix/FillTheMatrix.cs
@@ -3,10 +3,10 @@ Problem 1. Fill the matrix
 Write a program that fills and prints a matrix of size (n, n) as shown below:
 Example for n=4:
 a)               b)              c)              d)
- 1	5	9	13   1	8	9	16   7	11	14	16   1	12	11	10
- 2	6   10	14   2	7	10	15   4	8	12	15   2	13	16	9
- 3	7	11	15   3	6	11	14   2	5	9	13   3	14	15	8
- 4	8	12	16   4	5	12	13   1	3	6	10   4	5	6	7
+ 1	5	9	13    1	8	9	16    7	11	14	16   1	12	11	10
+ 2	6 10	14   2	7	10	15   4	8	12	15    2	13	16	9
+ 3	7	11	15   3	6	11	14   2	5	9	13     3	14	15	8
+ 4	8	12	16   4	5	12	13   1	3	6	10     4	5	6	7
 */
 
 using System;
